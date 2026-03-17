3107ecb74ac53afe8ff3685fc41ea878634a94de
devanshi599 <da5122@srmist.edu.in>
2026-03-17 08:53:23 +0530

    Implemented UC3: Add command -line argument support with defalt value

--- HelloApp.java
+++ HelloApp.java
@@ -1,13 +1,14 @@
 public class HelloApp {
     public static void main(String[] args) {
-        // args[0] captures the first word typed after the run command
+        String name;
+
+        
         if (args.length > 0) {
-            System.out.println("Hello, " + args[0] + "!");
+            name = args[0];
         } else {
-            System.out.println("Hello, World !");
-
-
-            
+            name = "World";
         }
+
+        System.out.println("Hello, " + name + "!");
     }
 }
